f7c1f4bee028624f300edb7fdced5a7be06de430
quangnv58 <quang0915057307@gmail.com>
2015-02-25 01:25:50 +0700

    layout
    beautiful

--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -1,26 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<base href="<?php echo base_url();?>"></base>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-	<link href="<?php echo base_url();?>public/frontend/css/layout.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>public/frontend/css/css/style.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>public/frontend/css/css/table.css" rel="stylesheet">
-	<link href="<?php echo base_url();?>public/frontend/css/css2/style.css" rel="stylesheet">
-	<style type="text/css"></style>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <base href=" <?php echo base_url();?> "></base>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <link href=" <?php echo base_url();?> public/frontend/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+  <title>Sinh viên cần</title>
 </head>
+<div class="container-fluid">
+	<div class="mainmenu">
+    <?php $this->load->view('frontend/modules/menu.php');?>
+  </div>
+  <div class="container">
+		<?php $this->load->view('frontend/modules/slide.php');?>
+    <?php $this->load->view('frontend/modules/boxtin.php');?>
+    <?php $this->load->view('frontend/modules/footer.php');?>		
+  </div>
+</div><!--/*end container*/-->
 <body>
-<div id="wrapper">
-	<div id="wrapper">
-    	<div id="header">
-        	<?php $this->load->view('frontend/modules/header.php');?>
-        </div>
-        <div id="content">
-        </div>
-        <div id="footer">
-		</div>
-	</div>
-</div>
 </body>
 </html>
--- application/views/frontend/modules/footer.php
+++ application/views/frontend/modules/footer.php
@@ -1,5 +1,30 @@
-		<div id="footer">
-			<p>
-				CopyRight by 111B-IT
-			</p>
+<div class="footer">
+	<div class="col-md-6">
+		<div class="embed-responsive embed-responsive-16by9">
+        	<div class="jumbotron">
+  				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/AHrUsRVE_2Y" autoplay></iframe>
+                <div class="pull-bottom">
+                <p>Bạn có thích video này ?</p>
+                <div class="radio">
+                    <label><input type="radio" name="optradio" />Yes</label>
+                </div>
+                <div class="radio">
+                    <label><input type="radio" name="optradio"/>No</label>
+                </div>
+             </div>
+            </div>
+            
 		</div>
+    </div>
+    <div class="col-md-6 pull-right">
+        <div class="jumbotron">
+        	<h1>Love in work</h1>
+            <p>1.Làm điều bạn thích là tự do. Thích điều bạn làm là hạnh phúc.</p>
+            <p>2.Người phụ nữ có thể tự tạo công việc cho mình là người phụ nữ sẽ có được danh vọng và vận mệnh.</p>
+            <p>3.Khi bạn yêu thích công việc của mình, ngày nào cũng là ngày lễ.</p>
+        </div>
+    </div>
+    <div class="col-md-12">
+    <p style="text-align:center">Copy Right by 111B-IT</p>
+    </div>
+</div>
